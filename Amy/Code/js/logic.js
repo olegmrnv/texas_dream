e3415433288eb45f98bae5ead2e8a2ecfd70d080
AFR5057 <afr5057@gmail.com>
2019-06-13 21:33:24 -0500

    test code

--- Amy/Code/js/logic.js
+++ Amy/Code/js/logic.js
@@ -8,37 +8,40 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/til
 
 // Initialize all of the LayerGroups we'll be using
 var layers = {
-  COMING_SOON: new L.LayerGroup(),
-  EMPTY: new L.LayerGroup(),
-  LOW: new L.LayerGroup(),
-  NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
+  EMPLOYMENT: new L.LayerGroup(),
+  INCOME: new L.LayerGroup(),
+  SCHOOL_RATING: new L.LayerGroup(),
+  Age_Range: new L.LayerGroup(),
+  POLUTION: new L.LayerGroup(),
+  Allergens: new L.LayerGroup()
 };
 
 // Create the map with our layers
 var map = L.map("map-id", {
-  center: [31.9689, -99.9018],
-  zoom: 12,
-  // layers: [
-  //   layers.COMING_SOON,
-  //   layers.EMPTY,
-  //   layers.LOW,
-  //   layers.NORMAL,
-  //   layers.OUT_OF_ORDER
-  // ]
+  center: [31.4274, -94.8085],
+  zoom: 5,
+  layers: [
+    layers.EMPLOYMENT,
+    layers.INCOME,
+    layers.SCHOOL_RATING,
+    layers.Age_Range,
+    layers.POLUTION,
+    layers.Allergens
+  ]
 });
 
 // Add our 'lightmap' tile layer to the map
 lightmap.addTo(map);
 
 // Create an overlays object to add to the layer control
-// var overlays = {
-//   "Coming Soon": layers.COMING_SOON,
-//   "Empty Stations": layers.EMPTY,
-//   "Low Stations": layers.LOW,
-//   "Healthy Stations": layers.NORMAL,
-//   "Out of Order": layers.OUT_OF_ORDER
-// };
+var overlays = {
+  "Employment": layers.EMPLOYMENT,
+  "Income": layers.INCOME,
+  "School Rating": layers.SCHOOL_RATING,
+  "Age Range": layers.Age_Range,
+  "Polution": layers.POLUTION,
+  "Allergens": layers.Allergens,
+};
 
 // Create a control for our layers, add our overlay layers to it
 L.control.layers(null, overlays).addTo(map);
@@ -49,7 +52,7 @@ var info = L.control({
 });
 
 // When the layer control is added, insert a div with the class of "legend"
-info.onAdd = function() {
+info.onAdd = function () {
   var div = L.DomUtil.create("div", "legend");
   return div;
 };
@@ -58,112 +61,120 @@ info.addTo(map);
 
 // Initialize an object containing icons for each layer group
 var icons = {
-  COMING_SOON: L.ExtraMarkers.icon({
+  EMPLOYMENT: L.ExtraMarkers.icon({
     icon: "ion-settings",
     iconColor: "white",
     markerColor: "yellow",
     shape: "star"
   }),
-  EMPTY: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
+  INCOME: L.ExtraMarkers.icon({
+    icon: "ion-settings",
+    iconColor: "white",
+    markerColor: "pink",
+    shape: "star"
+  }),
+  SCHOOL_RATING: L.ExtraMarkers.icon({
+    icon: "ion-settings",
     iconColor: "white",
     markerColor: "red",
     shape: "circle"
   }),
-  OUT_OF_ORDER: L.ExtraMarkers.icon({
-    icon: "ion-minus-circled",
+  Age_Range: L.ExtraMarkers.icon({
+    icon: "ion-settings",
     iconColor: "white",
     markerColor: "blue-dark",
-    shape: "penta"
+    shape: "circle"
   }),
-  LOW: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
+  POLUTION: L.ExtraMarkers.icon({
+    icon: "ion-settings",
     iconColor: "white",
     markerColor: "orange",
-    shape: "circle"
+    shape: "penta"
   }),
-  NORMAL: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
+  Allergens: L.ExtraMarkers.icon({
+    icon: "ion-settings",
     iconColor: "white",
     markerColor: "green",
-    shape: "circle"
+    shape: "penta"
   })
 };
 
-// Perform an API call to the Citi Bike Station Information endpoint
-d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", function(infoRes) {
+// Perform an API call 
+d3.json("http://feeds.airnowapi.org/rss/realtime/903.xml", function (infoRes) {
 
-  // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
-  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json", function(statusRes) {
+  // When the first API call is complete, perform another call 
+  d3.json("http://feeds.airnowapi.org/rss/realtime/903.xml", function (statusRes) {
     var updatedAt = infoRes.last_updated;
-    var stationStatus = statusRes.data.stations;
+    vartatus = statusRes.data.stations;
     var stationInfo = infoRes.data.stations;
 
     // Create an object to keep of the number of markers in each layer
-    var stationCount = {
-      COMING_SOON: 0,
-      EMPTY: 0,
-      LOW: 0,
-      NORMAL: 0,
-      OUT_OF_ORDER: 0
+    var parameterCount = {
+      EMPLOYMENT: 0,
+      INCOME: 0,
+      SCHOOL_RATING: 0,
+      Age_Range: 0,
+      POLUTION: 0,
+      Allergens: 0,
     };
 
-    // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
-    var stationStatusCode;
+    // Initialize atatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
+    // vartatusCode;
 
-    // Loop through the stations (they're the same size and have partially matching data)
-    for (var i = 0; i < stationInfo.length; i++) {
+    // Loop through the (they're the same size and have partially matching data)
+    // for (var i = 0; i < stationInfo.length; i++) {
 
       // Create a new station object with properties of both station objects
-      var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-      // If a station is listed but not installed, it's coming soon
-      if (!station.is_installed) {
-        stationStatusCode = "COMING_SOON";
-      }
-      // If a station has no bikes available, it's empty
-      else if (!station.num_bikes_available) {
-        stationStatusCode = "EMPTY";
-      }
-      // If a station is installed but isn't renting, it's out of order
-      else if (station.is_installed && !station.is_renting) {
-        stationStatusCode = "OUT_OF_ORDER";
-      }
-      // If a station has less than 5 bikes, it's status is low
-      else if (station.num_bikes_available < 5) {
-        stationStatusCode = "LOW";
-      }
-      // Otherwise the station is normal
-      else {
-        stationStatusCode = "NORMAL";
-      }
+      // var station = Object.assign({}, stationInfo[i],tatus[i]);
+      // If a station is listed but not installed, it's Employment
+      // if (!station.is_installed) {
+      //  tatusCode = "EMPLOYMENT";
+      // }
+      // If a station has no bikes available, it's SCHOOL_RATING
+      // else if (!station.num_bikes_available) {
+      //  tatusCode = "SCHOOL_RATING";
+      // }
+      // If a station is installed but isn't renting, it's Age_Range
+      // else if (station.is_installed && !station.is_renting) {
+      //  tatusCode = "Age_Range";
+      // }
+      // If a station has less than 5 bikes, it's status is POLUTION
+      // else if (station.num_bikes_available < 5) {
+      //  tatusCode = "POLUTION";
+      // }
+      // Otherwise the station is Allergens
+      // else {
+      //  tatusCode = "Allergens";
+      // }
 
       // Update the station count
-      stationCount[stationStatusCode]++;
+      // parameterCount[stationStatusCode]++;
       // Create a new marker with the appropriate icon and coordinates
-      var newMarker = L.marker([station.lat, station.lon], {
-        icon: icons[stationStatusCode]
-      });
+      // var newMarker = L.marker([station.lat, station.lon], {
+        // icon: icons[stationStatusCode]
+      // });
 
       // Add the new marker to the appropriate layer
-      newMarker.addTo(layers[stationStatusCode]);
+      // newMarker.addTo(layers[stationStatusCode]);
 
       // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-      newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-    }
+      // newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
+    // }
 
     // Call the updateLegend function, which will... update the legend!
-    updateLegend(updatedAt, stationCount);
+    updateLegend(updatedAt, parameterCount);
   });
 });
 
 // Update the legend's innerHTML with the last updated time and station count
-function updateLegend(time, stationCount) {
+function updateLegend(time, parameterCount) {
   document.querySelector(".legend").innerHTML = [
     "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
+    "<p class='INCOME'>Income: " + parameterCount.Age_Range + "</p>",
+    "<p class='age_range'>Age_Range: " + parameterCount.Age_Range + "</p>",
+    "<p class='employment'>Employment: " + parameterCount.EMPLOYMENT + "</p>",
+    "<p class='SCHOOL_RATING'>SCHOOL_RATING: " + parameterCount.SCHOOL_RATING + "</p>",
+    "<p class='POLUTION'>POLUTION: " + parameterCount.POLUTION + "</p>",
+    "<p class='Allergens'>ALLERGENS: " + parameterCount.Allergens + "</p>"
   ].join("");
 }
