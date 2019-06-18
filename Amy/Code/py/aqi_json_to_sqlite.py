97ace5080cac6664a709bb0c7ae0a0bddbd50ec7
AFR5057 <afr5057@gmail.com>
2019-06-17 19:52:23 -0500

    updates

--- Amy/Code/py/aqi_json_to_sqlite.py
+++ Amy/Code/py/aqi_json_to_sqlite.py
@@ -1,22 +1,21 @@
 import os
 import sys
 import json
+import pandas as pd
 from datetime import datetime
 from os.path import expanduser
 import urllib3
 from config import API_KEY
-# create crosswalk for zip code / county to aqi station
-# load in sqllite
-# link zip code
+# create today date variable
 
 def main():
 
     # API parameters
     options = {}
     options["url"] = "https://airnowapi.org/aq/data/"
-    options["start_date"] = "2019-06-16"
+    options["start_date"] = "2019-06-17"
     options["start_hour_utc"] = "15"
-    options["end_date"] = "2019-06-16"
+    options["end_date"] = "2019-06-17"
     options["end_hour_utc"] = "23"
     options["parameters"] = "o3,pm25"
     options["bbox"] = "-107.027060,25.632792,-93.228232,36.731120"
@@ -55,10 +54,43 @@ def main():
         # Download complete
         print ("Download URL: %s" % REQUEST_URL)
         print ("Download File: %s" % resp.data)
+        return resp.data
 
     except Exception as e:
         print ("Unable perform AirNowAPI request. %s" % e)
         sys.exit(1)
-
-if __name__ == "__main__":
+        if __name__ == "__main__":
     main()
+
+    x = main()
+    output_aqi = pd.read_json(x)
+    output_aqi
+    
+DATABASE = "aqi.sqlite"
+
+c.execute('''CREATE TABLE IF NOT EXISTS "output" (
+	"site_autoid"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"Longitude"	NUMERIC,
+	"Latitude"	NUMERIC,
+	"UTC"	TEXT,
+	"Parameter"	TEXT,
+	"Unit"	TEXT,
+	"Value"	NUMERIC,
+	"AQI"	NUMERIC,
+	"Category"	NUMERIC,
+	"SiteName"	TEXT,
+	"AgencyName"	TEXT,
+	"FullAQSCode"	NUMERIC,
+	"IntlAQSCode"	NUMERIC
+)''')
+                               
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///aqi.sqlite', echo=False)
+from sqlalchemy import inspect
+inspector = inspect(engine)
+
+for table_name in inspector.get_table_names():
+    print(table_name)
+
+  output_aqi.columns  
+  output_aqi.to_sql('output', con=engine, if_exists='append', index=False)
