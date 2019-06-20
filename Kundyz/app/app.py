54e7bc641056f397b0fc842f3157a282fec1eee4
kundyz.smith@gmail.com <kundyz.smith@gmail.com>
2019-06-20 17:17:40 -0500

    more

--- Kundyz/app/app.py
+++ Kundyz/app/app.py
@@ -11,120 +11,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/tx_school_data.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/tx_data.sqlite"
 db = SQLAlchemy(app)
 
 Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
-# engine = create_engine("sqlite:///db/tx_school_data.sqlite")
+# engine = create_engine("sqlite:///db/tx_data.sqlite")
 # inspector = inspect(engine)
 
-income = Base.classes.Per_Capita_Personal_Income
-unemployment = Base.classes.Unemployment_Rate
-population = Base.classes.population
-
+school_ratings = Base.classes.combine_schools_zip_geo
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
-@app.route("/counties")
-def counties():
+@app.route("/ratings")
+def ratings():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(unemployment).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    return jsonify(list(df.County))
-
-
-@app.route("/unemp")
-def unemp():
-    """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(unemployment).statement
+    stmt = db.session.query(school_ratings).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (sample names)
-    my_dict = dict(zip(list(df.County), list(df.Unemployment)))
+    my_dict = dict(zip(list(df.SchoolType), list(df.CampusName), list(df.DistrictName), list(df.TotalScore), list(df.OverallGrade)))
     return jsonify(my_dict)
-
-@app.route("/income")
-def inc():
-    """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(income).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    my_dict = dict(zip(list(df.County), list(df.Income)))
-    return jsonify(my_dict)
-
-
-@app.route("/population")
-def popul():
-    """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(population).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    my_dict = dict(zip(list(df.County), list(df.Population)))
-    return jsonify(my_dict)
-
-
-# @app.route("/metadata/<sample>")
-# def sample_metadata(sample):
-#     """Return the MetaData for a given sample."""
-#     sel = [
-#         Samples_Metadata.sample,
-#         Samples_Metadata.ETHNICITY,
-#         Samples_Metadata.GENDER,
-#         Samples_Metadata.AGE,
-#         Samples_Metadata.LOCATION,
-#         Samples_Metadata.BBTYPE,
-#         Samples_Metadata.WFREQ,
-#     ]
-
-#     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-#     # Create a dictionary entry for each row of metadata information
-#     sample_metadata = {}
-#     for result in results:
-#         sample_metadata["sample"] = result[0]
-#         sample_metadata["ETHNICITY"] = result[1]
-#         sample_metadata["GENDER"] = result[2]
-#         sample_metadata["AGE"] = result[3]
-#         sample_metadata["LOCATION"] = result[4]
-#         sample_metadata["BBTYPE"] = result[5]
-#         sample_metadata["WFREQ"] = result[6]
-
-#     print(sample_metadata)
-#     return jsonify(sample_metadata)
-
-
-# @app.route("/samples/<sample>")
-# def samples(sample):
-#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-#     stmt = db.session.query(Samples).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Filter the data based on the sample number and
-#     # only keep rows with values above 1
-#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-#     # Format the data to send as json
-#     data = {
-#         "otu_ids": sample_data.otu_id.values.tolist(),
-#         "sample_values": sample_data[sample].values.tolist(),
-#         "otu_labels": sample_data.otu_label.tolist(),
-#     }
-#     return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
